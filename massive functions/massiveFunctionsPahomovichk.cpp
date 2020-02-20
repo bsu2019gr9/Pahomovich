8fc2a911ea545e7eee3277572d85569c13195fd1
movichksu <56875261+movichksu@users.noreply.github.com>
2020-02-20 23:27:01 +0300

    20.02.20

--- massive functions/massiveFunctionsPahomovichk.cpp
+++ massive functions/massiveFunctionsPahomovichk.cpp
@@ -5,6 +5,176 @@
 
 using namespace std;
 
+int* giveMemory(int N)
+{
+	int* Arr = nullptr;
+	Arr = new int[N];
+	return Arr;
+}
+void freeMemory(int*& Arr)// если передавать так, то после функции нужно очистить указатель; лучше передать по ссылке, чтобы поменять 
+{
+	delete[] Arr; Arr = nullptr;
+
+}
+
+void printArray(int* beg, int* end) {
+	for (int *p = beg; p < end; ++p)
+	cout <<setw(3)<< *p << " ";
+	cout << '\n';
+}
+void inputArray(int* beg, int* end) {
+	for (int *p = beg; p < end; ++p)
+		cin >> *p;
+}
+
+void initRandomArray(int* beg, int* end, const int m = -10, const int M = 10) {
+	int d = (M - m + 1);
+	for (int* p = beg; p < end; ++p)
+		*p = rand() % d + m;
+};
+
+void initArray(int* beg, int* end)
+{
+	int i = 1;
+	for (int* p = beg; p < end; ++p) 
+	{
+		*p = i; 
+		++i;
+	}
+}
+
+void initRandArray(int* beg, int* end)
+{
+	srand(time(NULL));
+	for (int *p = beg; p < end; ++p)
+		*p = 0 + rand() % 100;
+}
+
+int findSum(int* beg, int* end) {
+	int s = 0;
+	for (int* p = beg; p < end; ++p)
+		s += *p;
+	return s;
+}
+
+int findMax(int* beg, int* end) {
+	int max = *beg;
+	for (int* p = beg; p < end; ++p)
+		if (*p > max) max = *p;
+	return max;
+}
+int findMin(int* beg, int* end) {
+	int min = *beg;
+	for (int* p = beg; p < end; ++p)
+		if (*p < min)min = *p;
+	return min;
+}
+
+void reverse(int* beg, int* end)
+{
+
+	for (int* p = beg, *q = end - 1; p < q; ++p, --q)
+	{
+		swap(*p, *q);
+	}
+}
+
+void shuffleArr(int* beg, int* end)
+{
+	reverse(beg, end);
+	reverse(beg, end - (end - beg) / 3);
+	reverse(beg, end - (end - beg) / 2);
+	reverse(beg, (end - (end - beg) / 4));
+}
+
+void swap(int& a, int& b)
+{
+	int t;
+	t = a;
+	a = b;
+	b = t;
+}
+
+void initSortedArr(int* beg, int* end)
+{
+	int nuch = 10, num = 0;
+	srand(time(NULL));
+	for (int* p = beg; p < end; ++p)
+	{
+		num = rand() % (nuch)+0;
+		if (num >= *(p - 1))   *p = num;
+		else --* p;
+		nuch = *p + 5;
+	}
+}
+
+void moveLeft(int* beg, int* end, const int& k)
+{
+	reverse(beg, beg + k);
+	reverse(beg, end);
+	reverse(beg, end - k);
+}
+void moveRight(int* beg, int* end, const int& k)
+{
+	reverse(beg, end - k);
+	reverse(beg, end);
+	reverse(beg, beg + k);
+}
+
+
+void findMaxMin(int* beg, int* end, int& min, int& max) {
+
+	for (int* p = beg; p < end; ++p)
+	{
+		if (*p < min) min = *p;
+		if (*p > max)max = *p;
+	}
+}
+
+void copyArr(int* RHSbeg, int* RHSend, int* LHSbeg, int* LHSend) // копирование массива 
+{
+	if (RHSend - RHSbeg == LHSend - RHSbeg) {
+		for (int* Lp = LHSbeg, *Rp = RHSbeg; Lp < LHSend; ++Lp, ++Rp)
+			*Lp = *Rp;
+	}
+}
+
+void bubbleSort(int* beg, int* end)
+{
+	for (int j = 0; j < (end - beg) - 1; ++j)
+		for (int* p = beg; p < end - 1; ++p)
+		{
+			if (*p > * (p + 1))
+			{
+				swap(*p, *(p + 1));
+			}
+		}
+}
+
+bool areEqual(int* RHSbeg, int* RHSend, int* LHSbeg, int* LHSend) // проверка массивов на совпадение элементов
+{
+	if (RHSend - RHSbeg != LHSend - RHSbeg) return false;
+	else
+	{
+		for (int* Lp = LHSbeg, *Rp = RHSbeg; Lp < LHSend; ++Lp, ++Rp)
+			if (*Lp != *Rp) return false;
+		return true;
+	}
+}
+
+void shakeArr(int* beg, int* end)
+{
+	int k = 0;
+	srand(time(NULL));
+	for (int* p = beg; p < end; ++p)
+	{
+		k = rand() % (end - beg);
+		swap(*p, *(beg + k));
+	}
+}
+
+/*-----------------------old way--------------------------*/
+/*----------------------- it will be done... --------------------------*/
 
 int findIdMin(int* arr, const int& N) {
 	int minId = 0;
@@ -15,132 +185,6 @@ int findIdMax(int* arr, const int& N) {
 	int maxId = 0;
 	for (int i = 1; i < N; i++)if (arr[i] > arr[maxId])maxId = i;
 	return maxId;
-}
-void printArray(int* arr, const int& N) {
-	for (int i = 0; i < N; ++i)cout << arr[i] << " ";
-	cout << '\n';
-}
-void inputArray(int* arr, const int& N) {
-	for (int i = 0; i < N; ++i)cin >> arr[i];
-}
-
-/*void initArray(int* Arr, const int& N)
-{
-	int k;
-	srand(time(0));
-	cout << "your k for [-k; k] ? \n";
-	cin >> k;
-	for (int i = 0; i < N; ++i)  Arr[i] = rand() % (2 * k + 1) - k;
-}*/
-
-void initArray(int* Arr, const int& N)
-{
-	for (int i = 0; i < N; ++i)  Arr[i] = i;
-}
-
-void initRandArray(int* arr, const int N)
-{
-	srand(time(NULL));
-	for (int i = 0; i < N; ++i)
-		arr[i] = 0 + rand() % 100;
-}
-
-void initSortedArr(int* Arr, const int& N)
-{
-	int nuch = 10, num = 0;
-	srand(time(NULL));
-	for (int i = 0; i < N; ++i)
-	{
-		num = rand() % (nuch)+0;
-		if (num >= Arr[i - 1])   Arr[i] = num;
-		else --i;
-		nuch = Arr[i] + 5;
-	}
-}
-
-int findMax(int* arr, const int& N) {
-	int max = arr[0];
-	for (int i = 0; i < N; i++)if (arr[i] > max)max = arr[i];
-	return max;
-}
-int findMin(int* arr, const int N) {
-	int min = arr[0];
-	for (int i = 0; i < N; i++)if (arr[i] < min)min = arr[i];
-	return min;
-}
-void findMaxMin(int* arr, const int& N, int& min, int& max) {
-
-	for (int i = 0; i < N; i++) { if (arr[i] < min)min = arr[i]; if (arr[i] > max)max = arr[i]; }
-}
-int findSum(int* arr, const int N) {
-	int s = 0;
-	for (int i = 0; i < N; i++)
-		s += arr[i];
-	return s;
-}
-
-void reverce(int* Arr, const int& N)
-{
-	for (int i = 0; i < N / 2; ++i)
-	{
-		swap(Arr[i], Arr[N - 1 - i]);
-	}
-}
-
-void shuffleArr(int* Arr, const int& N)
-{
-	reverce(Arr, N / 4);
-	reverce(Arr, N / 2);
-	reverce(Arr, N);
-	reverce(Arr, N / 4);
-}
-void shakeArr(int* Arr, const int& N)
-{
-	int k = 0;
-	srand(time(NULL));
-	for (int i = 0; i < N; ++i)
-	{
-		k = rand() % N;
-		swap(Arr[i], Arr[k]);
-	}
-}
-void swap(int& a, int& b)
-{
-	int t;
-	t = a;
-	a = b;
-	b = t;
-}
-void copyArr(int* RHS, const int& NR, int* LHS, const int& NL) // копирование массива 
-{
-	if (NR == NL) {
-		for (int i = 0; i < NL; ++i)
-			LHS[i] = RHS[i];
-	}
-}
-
-
-void bubbleSort(int* Arr, const int& N)
-{
-	for (int j = 0; j < N - 1; ++j)
-		for (int i = 0; i < N - 1; ++i)
-		{
-			if (Arr[i] > Arr[i + 1])
-			{
-				swap(Arr[i], Arr[i + 1]);
-			}
-		}
-}
-
-bool areEqual(int* RHS, const int& NR, int* LHS, const int& NL) // проверка массивов на совпадение элементов
-{
-	if (NL != NR) return false;
-	else
-	{
-		for (int i = 0; i < NL; ++i)
-			if (LHS[i] != RHS[i]) return false;
-		return true;
-	}
 }
 
 void evenOddSort(int* Arr, const int& N)
@@ -201,10 +245,20 @@ void MargerArr(int* A, int* B, int* C, const int& m, const int& n)
 	for (k = 0; k < (n + m); ++k)
 		if (A[i] < B[j])
 		{
-			C[k] = A[i]; if (i < n) ++i; else { for (j = n - 1; j < m; ++j) { C[k] = B[j]; k++; }  break; }
+			C[k] = A[i]; 
+			if (i < n) ++i; 
+			else { 
+				for (j = n - 1; j < m; ++j) 
+				{ C[k] = B[j]; k++; }  
+				break; }
 		}
 		else {
-			C[k] = B[j]; if (j < m) ++j; else { for (i = m - 1; i < n; ++i) { C[k] = A[i]; k++; }  break; }
+			C[k] = B[j]; 
+			if (j < m) ++j; 
+			else 
+			{ for (i = m - 1; i < n; ++i) 
+			{ C[k] = A[i]; k++; }  
+			break; }
 		}
 }
 
@@ -217,7 +271,7 @@ int summIf(int* Arr, const int& n, int sum) {
 		if (Arr[i] > max) { max = Arr[i]; iM = i; }
 	}
 	cout << "\n last min: " << min << "first max: " << max << "\n";
-	if (im > iM&& im - 1 != iM)
+	if (im > iM && im - 1 != iM)
 	{
 		for (int i = iM + 1; i < im; ++i)
 		{
@@ -226,295 +280,4 @@ int summIf(int* Arr, const int& n, int sum) {
 		return sum;
 	}
 	else return 0;
-}
-
-void moveLeft(int* Arr, const int& n, const int& k)
-{
-	reverce(Arr, k);
-	reverce(Arr, n);
-	reverce(Arr, n - k);
-}
-void moveRight(int* Arr, const int& n, const int& k)
-{
-	reverce(Arr, n - k);
-	reverce(Arr, n);
-	reverce(Arr, k);
-}
-
-int* giveMemory(int N)
-{
-	int* Arr = nullptr;
-	Arr = new int[N];
-	return Arr;
-}
-void freeMemory(int* Arr)
-{
-	if (Arr)
-	{
-		delete[] Arr; Arr = nullptr;
-	}
-	else
-	{
-		cout << "Array is already empty."; return;
-	}
-}
-#include <iostream>
-#include <iomanip>
-#include<ctime>
-#include "massiveFunctionsPahomovichk.h"
-
-using namespace std;
-
-
-int findIdMin(int* arr, const int& N) {
-	int minId = 0;
-	for (int i = 1; i < N; i++)if (arr[i] < arr[minId])minId = i;
-	return minId;
-}
-int findIdMax(int* arr, const int& N) {
-	int maxId = 0;
-	for (int i = 1; i < N; i++)if (arr[i] > arr[maxId])maxId = i;
-	return maxId;
-}
-void printArray(int* arr, const int& N) {
-	for (int i = 0; i < N; ++i)cout << arr[i] << " ";
-	cout << '\n';
-}
-void inputArray(int* arr, const int& N) {
-	for (int i = 0; i < N; ++i)cin >> arr[i];
-}
-
-/*void initArray(int* Arr, const int& N)
-{
-	int k;
-	srand(time(0));
-	cout << "your k for [-k; k] ? \n";
-	cin >> k;
-	for (int i = 0; i < N; ++i)  Arr[i] = rand() % (2 * k + 1) - k;
-}*/
-
-void initArray(int* Arr, const int& N)
-{
-	for (int i = 0; i < N; ++i)  Arr[i] = i;
-}
-
-void initRandArray(int* arr, const int N)
-{
-	srand(time(NULL));
-	for (int i = 0; i < N; ++i)
-		arr[i] = 0 + rand() % 100;
-}
-
-void initSortedArr(int* Arr, const int& N)
-{
-	int nuch = 10, num = 0;
-	srand(time(NULL));
-	for (int i = 0; i < N; ++i)
-	{
-		num = rand() % (nuch)+0;
-		if (num >= Arr[i - 1])   Arr[i] = num;
-		else --i;
-		nuch = Arr[i] + 5;
-	}
-}
-
-int findMax(int* arr, const int& N) {
-	int max = arr[0];
-	for (int i = 0; i < N; i++)if (arr[i] > max)max = arr[i];
-	return max;
-}
-int findMin(int* arr, const int N) {
-	int min = arr[0];
-	for (int i = 0; i < N; i++)if (arr[i] < min)min = arr[i];
-	return min;
-}
-void findMaxMin(int* arr, const int& N, int& min, int& max) {
-
-	for (int i = 0; i < N; i++) { if (arr[i] < min)min = arr[i]; if (arr[i] > max)max = arr[i]; }
-}
-int findSum(int* arr, const int N) {
-	int s = 0;
-	for (int i = 0; i < N; i++)
-		s += arr[i];
-	return s;
-}
-
-void reverce(int* Arr, const int& N)
-{
-	for (int i = 0; i < N / 2; ++i)
-	{
-		swap(Arr[i], Arr[N - 1 - i]);
-	}
-}
-
-void shuffleArr(int* Arr, const int& N)
-{
-	reverce(Arr, N / 4);
-	reverce(Arr, N / 2);
-	reverce(Arr, N);
-	reverce(Arr, N / 4);
-}
-void shakeArr(int* Arr, const int& N)
-{
-	int k = 0;
-	srand(time(NULL));
-	for (int i = 0; i < N; ++i)
-	{
-		k = rand() % N;
-		swap(Arr[i], Arr[k]);
-	}
-}
-void swap(int& a, int& b)
-{
-	int t;
-	t = a;
-	a = b;
-	b = t;
-}
-void copyArr(int* RHS, const int& NR, int* LHS, const int& NL) // копирование массива 
-{
-	if (NR == NL) {
-		for (int i = 0; i < NL; ++i)
-			LHS[i] = RHS[i];
-	}
-}
-
-
-void bubbleSort(int* Arr, const int& N)
-{
-	for (int j = 0; j < N - 1; ++j)
-		for (int i = 0; i < N - 1; ++i)
-		{
-			if (Arr[i] > Arr[i + 1])
-			{
-				swap(Arr[i], Arr[i + 1]);
-			}
-		}
-}
-
-bool areEqual(int* RHS, const int& NR, int* LHS, const int& NL) // проверка массивов на совпадение элементов
-{
-	if (NL != NR) return false;
-	else
-	{
-		for (int i = 0; i < NL; ++i)
-			if (LHS[i] != RHS[i]) return false;
-		return true;
-	}
-}
-
-void evenOddSort(int* Arr, const int& N)
-{
-	int i = 0, j = 0, cnt = 1;
-	while (cnt)
-	{
-		cnt = 0;
-		if (N % 2 == 0) {
-			for (i = 0; i < N - 1; i += 2)
-				if (Arr[i] > Arr[i + 1]) { swap(Arr[i], Arr[i + 1]); ++cnt; }
-
-			for (j = 1; j < N - 2; j += 2)
-				if (Arr[j] > Arr[j + 1]) { swap(Arr[j], Arr[j + 1]); ++cnt; }
-		}
-		else
-		{
-			for (i = 0; i < N - 2; i += 2)
-				if (Arr[i] > Arr[i + 1]) { swap(Arr[i], Arr[i + 1]); ++cnt; }
-
-			for (j = 1; j < N - 1; j += 2)
-				if (Arr[j] > Arr[j + 1]) { swap(Arr[j], Arr[j + 1]); ++cnt; }
-		}
-
-	}
-}
-
-void ShellSort(int* Arr, const int& N)
-{
-	int tmp;
-	int step = 0, i = 0, j = 0;
-	for (step = N / 2; step > 0; step /= 2) {
-		for (i = step; i < N; ++i) {
-			for (j = i - step; j >= 0 && Arr[j] > Arr[j + step]; j -= step)
-			{
-				swap(Arr[j], Arr[j + step]);
-			}
-		}
-	}
-}
-
-void CompressArr(int* Arr, const int& n, const int& a, const int& b)
-{
-	int i = 0, j = 0;
-	for (i = 0; i <= n; ++i)
-		if (fabs(Arr[i]) >= a && fabs(Arr[i]) <= b)
-		{
-			for (j = i; j < n - 1; j++)
-				Arr[j] = Arr[j + 1];
-			Arr[j] = 0;
-			i--;
-		}
-}
-
-void MargerArr(int* A, int* B, int* C, const int& m, const int& n)
-{
-	int i = 0, j = 0, k = 0;
-	for (k = 0; k < (n + m); ++k)
-		if (A[i] < B[j])
-		{
-			C[k] = A[i]; if (i < n) ++i; else { for (j = n - 1; j < m; ++j) { C[k] = B[j]; k++; }  break; }
-		}
-		else {
-			C[k] = B[j]; if (j < m) ++j; else { for (i = m - 1; i < n; ++i) { C[k] = A[i]; k++; }  break; }
-		}
-}
-
-int summIf(int* Arr, const int& n, int sum) {
-	int min, max;
-	min = max = Arr[0];
-	int im = 0, iM = 0;
-	for (int i = 0; i < n; i++) {
-		if (Arr[i] <= min) { min = Arr[i]; im = i; }
-		if (Arr[i] > max) { max = Arr[i]; iM = i; }
-	}
-	cout << "\n last min: " << min << "first max: " << max << "\n";
-	if (im > iM&& im - 1 != iM)
-	{
-		for (int i = iM + 1; i < im; ++i)
-		{
-			sum += Arr[i];
-		}
-		return sum;
-	}
-	else return 0;
-}
-
-void moveLeft(int* Arr, const int& n, const int& k)
-{
-	reverce(Arr, k);
-	reverce(Arr, n);
-	reverce(Arr, n - k);
-}
-void moveRight(int* Arr, const int& n, const int& k)
-{
-	reverce(Arr, n - k);
-	reverce(Arr, n);
-	reverce(Arr, k);
-}
-
-int* giveMemory(int N)
-{
-	int* Arr = nullptr;
-	Arr = new int[N];
-	return Arr;
-}
-void freeMemory(int* Arr)
-{
-	if (Arr)
-	{
-		delete[] Arr; Arr = nullptr;
-	}
-	else
-	{
-		cout << "Array is already empty."; return;
-	}
 }
--- massive functions/massiveFunctionsPahomovichk.h
+++ massive functions/massiveFunctionsPahomovichk.h
@@ -1,32 +1,32 @@
-int* giveMemory(int);
-void freeMemory(int*);
-void printArray(int*, const int&); // печать массива
-void inputArray(int*, const int&); //
-void initRandArray(int*, const int&); //инициализация элементов массива
-void initArray(int*, const int&);//генерирует случайные числа от -k до k
-void initSortedArr(int*, const int&); // вводит отсортированный массив рандомных чисел
+int* giveMemory(int); 
+void freeMemory(int*&);
+void printArray(int*, int* ); // печать массива
+void inputArray(int*, int*); //
+void initRandArray(int*, int*); //инициализация элементов массива
+void initArray(int*, int*);//генерирует случайные числа от -k до k
+void initSortedArr(int*, int*); // вводит отсортированный массив рандомных чисел
+void initRandomArray(int*, int*, const int, const int);
 
-int findMax(int*, const int&); // возвпащает максимальный элемент массива
-int findMin(int*, const int&); // возвращает минимальный элемент массива
-void findMaxMin(int*, const int&, int&, int&); // ищем одновременно максимальный и минимальный элементы массивов
-int findSum(int*, const int&); // возвращает сумму всех элементов массива
+int findMax(int*, int*); // возвпащает максимальный элемент массива
+int findMin(int*, int*); // возвращает минимальный элемент массива
+void findMaxMin(int*, int*, int&, int&); // ищем одновременно максимальный и минимальный элементы массивов
+int findSum(int*, int*); // возвращает сумму всех элементов массива
 int findIdMax(int*, const int&);
 int findIdMin(int*, const int&);
 
-void reverce(int*, const int&);//реверс массива
+void reverse(int*, int*);//реверс массива
 void swap(int&, int&);
 
+bool areEqual(int*, int*, int*, int*); // проверка эквивалентности соответствующих элементов массива (эквивалентности массивов)
+void copyArr(int*, int*, int*, int*); // копирует массив в другой массив
 
-bool areEqual(int*, const int&, int*, const int&); // проверка эквивалентности соответствующих элементов массива (эквивалентности массивов)
-void copyArr(int*, const int&, int*, const int&); // копирует массив в другой массив
-
-void bubbleSort(int*, const int&);// сортировка пузырьком
+void bubbleSort(int*, int*);// сортировка пузырьком
 void evenOddSort(int*, const int&); //чётно-нечётная сортировка
 void ShellSort(int*, const int&); // сортировка Шелла
-void shuffleArr(int*, const int&); //тасует элементы массива (несколько реверсов массива по частям)
-void shakeArr(int*, const int&); // перетасовка элементов массива (смена местами рандомных элементов) 
-void moveLeft(int*, const int&, const int&); // сдвиг массива на к элементов влево
-void moveRight(int*, const int&, const int&); //сдвиг массива на к элементов вправо
+void shuffleArr(int*, int*); //тасует элементы массива (несколько реверсов массива по частям)
+void shakeArr(int*, int*); // перетасовка элементов массива (смена местами рандомных элементов) 
+void moveLeft(int*, int*, const int&); // сдвиг массива на к элементов влево
+void moveRight(int*, int*, const int&); //сдвиг массива на к элементов вправо
 
 void MargerArr(int*, int*, int*, const int&, const int&);//слияние двух первых массивов А и В с размерами m и n соответственно в массив С размером в два раза больше максимального
 void CompressArr(int*, const int&, const int&, const int&); // сжатие массива. остаются значения в диапазоне [a;b]
